05e78e3fb59d8571cfc9d53bbdb6ef9c4e4db66b
fatemehprb <fatemepoorbagheri1382@yahoo.com>
2023-12-09 15:58:11 +0330

    cpp file was added

---
+++ tempMain.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	int T[8][8];
+	
+	
+	int J;
+	for (J = 0; J < 64; J++) {
+		if ((J % 8) % 2 == 0 and (J / 8) % 2 == 0) *(*(T + J / 8) + J % 8) = 0;
+		else if ((J % 8) % 2 == 1 and (J / 8) % 2 == 1) *(*(T + J / 8) + J % 8) = 0;
+		else *(*(T + J / 8) + J % 8) = 1;
+	}
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cout << T[i][j] << " ";
+
+		}
+		cout << endl;
+	}
+}
